e6d1e626f88d06e711e00ba67034e3f8c158ff73
gilbertosantana <gilbertosantoss307@gmail.com>
2022-12-21 16:13:17 -0300

    Dellete seller

--- SalesWebMvc60/Services/SellerService.cs
+++ SalesWebMvc60/Services/SellerService.cs
@@ -1,5 +1,6 @@
 ﻿using SalesWebMvc60.Data;
 using SalesWebMvc60.Models;
+using System.Linq;
 
 namespace SalesWebMvc60.Services
 {
@@ -20,6 +21,18 @@ namespace SalesWebMvc60.Services
         public void Insert(Seller obj)
         {
             _context.Add(obj);
+            _context.SaveChanges();
+        }
+
+        public Seller? FindById(int id)
+        {
+            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+        }
+        
+        public void Remove(int id)
+        {
+            var obj = _context.Seller.Find(id)!;
+            _context.Seller.Remove(obj);
             _context.SaveChanges();
         }
     }
