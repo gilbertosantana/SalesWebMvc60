33d8c475cea3ff6d02bb39a0b0fe400c2262b6de
gilbertosantana <gilbertosantoss307@gmail.com>
2022-12-22 09:04:30 -0300

    Seller details and eager loading

--- SalesWebMvc60/Services/SellerService.cs
+++ SalesWebMvc60/Services/SellerService.cs
@@ -1,4 +1,5 @@
-﻿using SalesWebMvc60.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using SalesWebMvc60.Data;
 using SalesWebMvc60.Models;
 using System.Linq;
 
@@ -26,7 +27,7 @@ namespace SalesWebMvc60.Services
 
         public Seller? FindById(int id)
         {
-            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+            return _context.Seller.Include(obj => obj.Department).FirstOrDefault(obj => obj.Id == id);
         }
         
         public void Remove(int id)
