46f04f42389ac4db275cf8a8bc8a2df1e45b8240
gilbertosantana <gilbertosantoss307@gmail.com>
2022-12-24 12:46:22 -0300

    Implementing simple search

---
+++ SalesWebMvc60/Controllers/SalesRecordsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SalesWebMvc60.Controllers
+{
+    public class SalesRecordsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult SimpleSearch()
+        {
+            return View();
+        }
+
+        public IActionResult GroupingSearch()
+        {
+            return View();
+        }
+    }
+}
