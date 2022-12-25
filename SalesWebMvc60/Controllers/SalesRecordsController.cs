2052cae496269e5fdc9fc67bcc0ee2734deae6f3
gilbertosantana <gilbertosantoss307@gmail.com>
2022-12-25 12:03:05 -0300

    Implementing grouping search

--- SalesWebMvc60/Controllers/SalesRecordsController.cs
+++ SalesWebMvc60/Controllers/SalesRecordsController.cs
@@ -34,9 +34,21 @@ namespace SalesWebMvc60.Controllers
             return View(result);
         }
 
-        public IActionResult GroupingSearch()
+        public async Task<IActionResult> GroupingSearch(DateTime? minDate, DateTime? maxDate)
         {
-            return View();
+            if (!minDate.HasValue)
+            {
+                minDate = new DateTime(DateTime.Now.Year, 1, 1);
+            }
+            if (!maxDate.HasValue)
+            {
+                maxDate = DateTime.Now;
+            }
+
+            ViewData["minDate"] = minDate.Value.ToString("yyyy-MM-dd");
+            ViewData["maxDate"] = maxDate.Value.ToString("yyyy-MM-dd");
+            var result = await _sallesRecordService.FindByDateGroupingAsync(minDate, maxDate);
+            return View(result);
         }
     }
 }
